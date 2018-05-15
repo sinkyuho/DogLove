9715e460586b9ad192b1e91ea2db95f0005333d2
sinkyuho <bb7305@naver.com>
2018-05-15 10:25:11 +0900

    html doglove

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <div class="hero">
+    <div class = "content">
+        <h1 class="title is-1" >lovedog</h1>
+        <h2 class="subtitle" >강아지를 사랑하는..</h2>
+      </div> 
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+</script>
